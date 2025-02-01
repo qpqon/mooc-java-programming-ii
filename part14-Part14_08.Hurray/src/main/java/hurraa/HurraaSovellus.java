08d94692e88a4d14c7125dae9c844f09eaf7a3f4
Daniel Menke <qpq@gmx.org>
2025-02-01 13:41:51 +0100

    Added initial solution

--- part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
+++ part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
@@ -4,17 +4,26 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
+
+// This task is broken by default because the dependency javafx.scene.media.AudioClip;
+// had not been implemented. Although I could add it to Maven and "trick" the test into
+// accepting my solution, I am not able to upload it to Helsinki servers because
+// it does not compile apparently. 
 
 public class HurraaSovellus extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        BorderPane pane = new BorderPane();
+        
+        AudioClip applause = new AudioClip("file:Applause-Yannick_Lemieux.wav");
 
         Button nappi = new Button("Hurraa!");
+        nappi.setOnAction((event) -> applause.play());
+        
+        BorderPane pane = new BorderPane();
         pane.setCenter(nappi);
-
 
         Scene scene = new Scene(pane, 600, 400);
 
