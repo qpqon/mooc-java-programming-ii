ec449dfcb8b605a9d94cffab16f37d173fc78cd0
Daniel Menke <qpq@gmx.org>
2025-02-02 18:08:49 +0100

    Added initial solution

--- part14-Part14_10.Database/src/main/java/application/UserInterface.java
+++ part14-Part14_10.Database/src/main/java/application/UserInterface.java
@@ -29,7 +29,37 @@ public class UserInterface {
                 break;
             }
 
-            // implement the functionality here
+            switch (command) {
+                case "1":
+                    System.out.println("Listing the database contents");
+                    System.out.println(database.list());
+                    break;
+                
+                case "2":
+                    System.out.println("Adding a new todo");
+                    System.out.println("Enter name");
+                    String todoName = scanner.nextLine();
+                    System.out.println("Enter description");
+                    String todoDescrip = scanner.nextLine();
+                    
+                    database.add(new Todo(todoName, todoDescrip, false));
+                    break;
+                    
+                case "3":
+                    System.out.println("Which todo should be marked as done (give the id)?");
+                    int idDone = Integer.parseInt(scanner.nextLine());            
+                    database.markAsDone(idDone);
+                    break;
+                    
+                case "4":
+                    System.out.println("Which todo should be removed (give the id)?");
+                    int idDelete = Integer.parseInt(scanner.nextLine());
+                    database.remove(idDelete);
+                    break;
+                    
+                default:
+                    System.out.println("Command unknown");
+            }
         }
 
         System.out.println("Thank you!");
